4007cc6f6b0361d7c5b7b1fcf8920c7114af854f
Glaydson Pacifico <glaydson.pacifico@gmail.com>
2024-03-17 10:37:45 -0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1">
+        Página Inicial
+      </Typography>
+    </>
+  )
+}
+
+export default App
